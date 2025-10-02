9b1bf9b51e802279335eb5d46542f9a03fccf72e
SmileJakoby <46690956+SmileJakoby@users.noreply.github.com>
2025-10-01 20:34:14 -0600

    router working. buttons still ugly

--- src/app.jsx
+++ src/app.jsx
@@ -44,3 +44,6 @@ export default function App() {
 </body>
 </BrowserRouter>);
 }
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+}
