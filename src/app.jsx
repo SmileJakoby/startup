1939bc39e889213c13f22eecf9d1abcd371f8350
SmileJakoby <46690956+SmileJakoby@users.noreply.github.com>
2025-10-01 19:53:00 -0600

    Why are the buttons so ugly why css no work

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <body>
+<header className="Navigation">
+            <form method="get" action="index.html">
+                <button type="submit" className = "SideButton" id="LogoutButton">Logout</button>
+            </form>
+            <form method="get" action="index.html">
+                <button type="submit" className = "SideButton" id="LoginButton">Login</button>
+            </form>
+            <form method="get" action="play.html">
+                <button type="submit" className = "SideButton" id="PlayButton">Play</button>
+            </form>
+            <form method="get" action="leaderboard.html">
+                <button type="submit" className = "SideButton" id="LeaderboardsButton">Leaderboards</button>
+            </form>
+            <form method="get" action="about.html">
+                <button type="submit" className = "SideButton" id="AboutButton">About</button>
+            </form>
+        </header>
+
+<main>App components go here</main>
+
+<footer>
+<a id="NameText">Jacob Skarda</a>
+<br id="RandomBR"/>
+<a id="GitLink" href="https://github.com/SmileJakoby/startup">Where the sun doesn't shine (My Github Repo)</a>
+</footer>
+</body>
 }
